86083832bce55559356c6b9fa387241de1de82c5
Tatiana Stewart <tstwrt7@gmail.com>
2024-07-10 19:15:51 -0400

    Group model created

---
+++ server/models/Group.py
@@ -0,0 +1,25 @@
+# Model for Groups for sharing recipes
+
+from server import db
+
+class Group(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    members = db.Column(db.String(500), nullable=False)
+    recipes = db.Column(db.String(500), nullable=False)
+    created_by = db.Column(db.String(100), nullable=False)
+
+    def __init__(self, name, members, recipes, created_by):
+        self.name = name
+        self.members = members
+        self.recipes = recipes
+        self.created_by = created_by
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'members': self.members,
+            'recipes': self.recipes,
+            'created_by': self.created_by
+        }
