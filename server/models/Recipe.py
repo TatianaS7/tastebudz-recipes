ea88cd5e0619c981b6ee1bf6f4ab362cef80ebcf
Tatiana Stewart <tstwrt7@gmail.com>
2024-07-10 19:09:36 -0400

    Created Recipe model

---
+++ server/models/Recipe.py
@@ -0,0 +1,38 @@
+# Model for Recipes
+
+from server import db
+
+class Recipe(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    # Ingredients should be a list of strings
+    ingredients = db.Column(db.String(500), nullable=False)
+    instructions = db.Column(db.String(500), nullable=False)
+    servings = db.Column(db.Integer, nullable=True)
+    time = db.Column(db.Integer, nullable=True)
+    image = db.Column(db.String(500), nullable=True)
+    tags = db.Column(db.String(500), nullable=False)
+    user = db.Column(db.String(100), nullable=False)
+
+    def __init__(self, name, ingredients, instructions, servings, time, image, tags, user):
+        self.name = name
+        self.ingredients = ingredients
+        self.instructions = instructions
+        self.servings = servings
+        self.time = time
+        self.image = image
+        self.tags = tags
+        self.user = user
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'ingredients': self.ingredients,
+            'instructions': self.instructions,
+            'servings': self.servings,
+            'time': self.time,
+            'image': self.image,
+            'tags': self.tags,
+            'user': self.user
+        }
