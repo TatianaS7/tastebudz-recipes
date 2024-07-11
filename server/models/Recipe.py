4a754c8e85b58db5e189a36bd53128e16b3ab93b
Tatiana Stewart <tstwrt7@gmail.com>
2024-07-10 20:18:27 -0400

    Created routes for recipes

--- server/models/Recipe.py
+++ server/models/Recipe.py
@@ -5,13 +5,12 @@ from connection import db
 class Recipe(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
-    # Ingredients should be a list of strings
-    ingredients = db.Column(db.String(500), nullable=False)
-    instructions = db.Column(db.String(500), nullable=False)
+    ingredients = db.Column(db.JSON, nullable=False)
+    instructions = db.Column(db.JSON, nullable=False)
     servings = db.Column(db.Integer, nullable=True)
     time = db.Column(db.Integer, nullable=True)
     image = db.Column(db.String(500), nullable=True)
-    tags = db.Column(db.String(500), nullable=False)
+    tags = db.Column(db.JSON, nullable=False)
     user = db.Column(db.String(100), nullable=False)
 
     def __init__(self, name, ingredients, instructions, servings, time, image, tags, user):
---
+++ server/routes/recipes.py
@@ -0,0 +1,97 @@
+from flask import Blueprint, request, jsonify
+from connection import db
+
+from models import Recipe
+
+recipes = Blueprint('recipes', __name__)
+
+# Create a Recipe
+@recipes.route('/', methods=['POST'])
+def create_recipe():
+    try:
+        data = request.get_json()
+        
+        if 'name' not in data or 'ingredients' not in data or 'instructions' not in data or 'tags' not in data or 'user' not in data:
+            return jsonify({'error': 'Missing required fields'})
+
+        new_recipe = Recipe(
+                name=data['name'],
+                ingredients=data['ingredients'],
+                instructions=data['instructions'],
+                servings= data.get('servings', None),
+                time= data.get('time', None),
+                image= data.get('image', None),
+                tags=data['tags'],
+                user=data['user']
+        )
+        db.session.add(new_recipe)
+        db.session.commit()
+        return jsonify(new_recipe.serialize())
+    except Exception as e:
+        return(str(e))
+
+
+# Get All Recipes
+@recipes.route('/all', methods=['GET'])
+def get_all_recipes():
+    try:
+        all_recipes = Recipe.query.all()
+        results = [recipe.serialize() for recipe in all_recipes]
+        return jsonify(results)
+    except Exception as e:
+        return(str(e))
+
+# Get All User Recipes
+@recipes.route('/user/<user>', methods=['GET'])
+def get_recipes_by_user(user):
+    try:
+        user_recipes = Recipe.query.filter_by(user=user).all()
+        results = [recipe.serialize() for recipe in user_recipes]
+        return jsonify(results)
+    except Exception as e:
+        return(str(e))
+    
+# Update a Recipe (has to be the user who created the recipe)
+@recipes.route('/<id>', methods=['PUT'])
+def update_recipe(id):
+    try:
+        recipe = Recipe.query.get(id)
+        data = request.get_json()
+        if recipe.user == data['user']:
+            if 'name' in data:
+                recipe.name = data['name']
+            if 'ingredients' in data:
+                recipe.ingredients = data['ingredients']
+            if 'instructions' in data:
+                recipe.instructions = data['instructions']
+            if 'servings' in data:
+                recipe.servings = data['servings']
+            if 'time' in data:
+                recipe.time = data['time']
+            if 'image' in data:
+                recipe.image = data['image']
+            if 'tags' in data:
+                recipe.tags = data['tags']
+            db.session.commit()
+            return jsonify(recipe.serialize())
+        else:
+            return jsonify({'error': 'You are not the owner of this recipe'})
+    except Exception as e:
+        return(str(e))
+    
+# Delete a Recipe (has to be the user who created the recipe)
+@recipes.route('/<id>', methods=['DELETE'])
+def delete_recipe(id):
+    try:
+        recipe = Recipe.query.get(id)
+        if recipe is None:
+            return jsonify({'error': 'Recipe not found'})
+        data = request.get_json()
+        if recipe.user == data['user']:
+            db.session.delete(recipe)
+            db.session.commit()
+            return jsonify({'message': 'Recipe deleted'})
+        else:
+            return jsonify({'error': 'You are not the owner of this recipe'})
+    except Exception as e:
+        return(str(e))
