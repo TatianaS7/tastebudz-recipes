437086e5ec1b3089308fb92adf92527df1b93895
Tatiana Stewart <tstwrt7@gmail.com>
2024-07-22 04:00:14 -0400

    Created routes for Groups and Group Recipes

---
+++ server/routes/groups.py
@@ -0,0 +1,260 @@
+from flask import Blueprint, request, jsonify
+from sqlalchemy.orm.attributes import flag_modified
+from connection import db
+
+from models import Group, Recipe
+
+groups = Blueprint('groups', __name__)
+
+# GROUP ROUTES
+# Create a Group (admin)
+@groups.route('/', methods=['POST'])
+def create_group():
+    try:
+        data = request.get_json()
+        
+        if 'name' not in data or 'admin' not in data:
+            return jsonify({'error': 'Missing required fields'})
+
+        new_group = Group(
+                name=data['name'],
+                members=data.get('members', None),
+                recipes=data.get('recipes', None),
+                admin=data['admin']
+        )
+        db.session.add(new_group)
+        db.session.commit()
+        return jsonify(new_group.serialize())
+    except Exception as e:
+        return(str(e))
+
+# Get a Group (any member)
+@groups.route('/<group_id>', methods=['GET'])
+def get_group(group_id):
+    try:
+        group = Group.query.get(group_id)
+        if not group:
+            return jsonify({'error': 'Group not found'})
+        
+        # Serialize Recipes to avoid type errors
+        recipes_data = [recipe.to_dict() for recipe in group.recipes]
+
+        # Create a dictionary to return the group data
+        group_data = {
+            'id': group.id,
+            'name': group.name,
+            'members': group.members,
+            'recipes': recipes_data,
+            'admin': group.admin
+        }
+
+        return jsonify(group_data)
+    except Exception as e:
+        return(str(e))
+
+# Get all Groups (any member)
+@groups.route('/all', methods=['GET'])
+def get_all_groups():
+    try:
+        groups = Group.query.all()
+        return jsonify([group.serialize() for group in groups])
+    except Exception as e:
+        return(str(e))
+
+# Join a Group (member)
+@groups.route('/join/<group_id>', methods=['PUT'])
+def join_group(group_id):
+    try:
+        group = Group.query.get(group_id)
+        if not group:
+            return jsonify({'error': 'Group not found'})
+        data = request.get_json()
+        if not data or 'email' not in data:
+            return jsonify({'error': 'Missing required fields'})
+        
+        for member in group.members:
+            if member['email'] == data['email']:
+                return jsonify({'error': 'User already in group'})
+        
+        new_member = { "email": data['email'], "role": "member" }
+        group.members.append(new_member)
+        flag_modified(group, 'members')
+        db.session.commit()
+
+        return jsonify(group.serialize()), 200
+    except Exception as e:
+        return(str(e))
+
+# Leave a Group (member)
+@groups.route('/leave/<group_id>', methods=['PUT'])
+def leave_group(group_id):
+    try:
+        group = Group.query.get(group_id)
+        data = request.get_json()
+        if not data or 'email' not in data:
+            return jsonify({'error': 'Email is required'})
+        
+        # Loop through the group's members to find the user
+        for member in group.members:
+            # If the user is present, and a member, remove them from the group
+            if member['email'] == data['email'] and member['role'] == 'member':
+                group.members.remove(member)
+                # Flag the group's members list as modified to recognize the change
+                flag_modified(group, 'members')
+                db.session.commit()
+                return jsonify(group.serialize())
+            
+            # If the user is an admin, they cannot leave the group
+            elif member['email'] == data['email'] and member['role'] == 'admin':
+                return jsonify({'error': 'Admin cannot leave group'})
+        
+        return jsonify({'error': 'User not in group'})
+    except Exception as e:
+        return(str(e))
+
+# Delete a Group (admin)
+@groups.route('/<group_id>', methods=['DELETE'])
+def delete_group(group_id):
+    try:
+        group = Group.query.get(group_id)
+        data = request.get_json()
+        if not data or 'email' not in data:
+            return jsonify({'error': 'Email is required'})
+        if group.admin == data['email']:
+            db.session.delete(group)
+            db.session.commit()
+            return jsonify({'message': 'Group deleted'})
+        else:
+            return jsonify({'error': 'Only the admin can delete this group'})
+    except Exception as e:
+        return(str(e))
+
+
+# RECIPES ROUTES
+# Add a Recipe to a Group (any member)
+@groups.route('/<group_id>/recipe', methods=['PUT'])
+def add_group_recipe(group_id):
+    try:
+        # Get the group
+        group = Group.query.get(group_id)
+        if not group:
+            return jsonify({'error': 'Group not found'})
+        # Get the recipe data from the request
+        data = request.get_json()
+        
+        # Check if required fields are present
+        for field in data:
+            if field not in ['name', 'ingredients', 'instructions', 'tags', 'user']:
+                return jsonify({'error': 'Missing required fields'})
+        
+        # Check if user is a member of the group
+        for member in group.members:
+            if member['email'] == data['user']:
+                break
+        else:
+            return jsonify({'error': 'Unauthorized user'})
+        
+        # Check if recipe name is unique
+        for recipe in group.recipes:
+            if recipe['name'] == data['name']:
+                return jsonify({'error': 'Recipe already exists; Try a different name'})
+            
+        # Create a new Recipe object
+        new_recipe = Recipe(
+            name=data['name'],
+            ingredients=data['ingredients'],
+            instructions=data['instructions'],
+            servings=data.get('servings', None),
+            time=data.get('time', None),
+            image=data.get('image', None),
+            tags=data['tags'],
+            user=data['user'],
+            group_id=group_id,
+            is_public=data.get('is_public', False)
+        )
+                
+        db.session.add(new_recipe)
+        db.session.commit()
+
+        recipes_data = [recipe.to_dict() for recipe in group.recipes]
+
+        return jsonify(recipes_data), 200
+    except Exception as e:
+        return(str(e))
+    
+# Get all Recipes in a Group (any member)
+@groups.route('/<group_id>/recipes/all', methods=['GET'])
+def get_group_recipes(group_id):
+    try:
+        group = Group.query.get(group_id)
+        if not group:
+            return jsonify({'error': 'Group not found'})
+        
+        recipes = Recipe.query.filter_by(group_id=group_id).all()
+        recipes_data = [recipe.to_dict() for recipe in recipes]
+
+        if not recipes_data:
+            return jsonify({'error': 'No recipes found'})
+
+        return jsonify(recipes_data)
+    except Exception as e:
+        return(str(e))
+
+# Update a Recipe in a Group (if created by the user or admin)
+@groups.route('/<group_id>/recipe/<recipe_id>', methods=['PUT'])
+def update_group_recipe(group_id, recipe_id):
+    try:
+            group = Group.query.get(group_id)
+            if not group:
+                return jsonify({'error': 'Group not found'})
+            
+            group_recipe = Recipe.query.get(recipe_id)
+            
+            if not group_recipe:
+                return jsonify({'error': 'Recipe not found'})
+            data = request.get_json()
+            if not data:
+                return jsonify({'error': 'Missing required fields'})
+            if group_recipe.user == data['email'] or data['email'] == Group.query.get(group_id).admin:
+                if 'name' in data:
+                    group_recipe.name = data['name']
+                if 'ingredients' in data:
+                    group_recipe.ingredients = data['ingredients']
+                if 'instructions' in data:
+                    group_recipe.instructions = data['instructions']
+                if 'servings' in data:
+                    group_recipe.servings = data['servings']
+                if 'time' in data:
+                    group_recipe.time = data['time']
+                db.session.commit()
+                return jsonify(group_recipe.serialize())
+            else:
+                return jsonify({'error': 'You are not the owner of this recipe'})
+    except Exception as e:
+        return(str(e))
+    
+# Delete a Recipe from a Group (if created by the user or admin)
+@groups.route('/<group_id>/recipe/<recipe_id>', methods=['DELETE'])
+def delete_group_recipe(group_id, recipe_id):
+    try:
+        group = Group.query.get(group_id)
+        if not group:
+            return jsonify({'error': 'Group not found'})
+        
+        group_recipe = next((recipe for recipe in group.recipes if str(recipe.id) == recipe_id), None)
+        
+        if not group_recipe:
+            return jsonify({'error': 'Recipe not found'})
+        
+        data = request.get_json()
+        if not data or 'email' not in data:
+            return jsonify({'error': 'Missing required fields'})
+        
+        if group_recipe.user == data['email'] or data['email'] == group.admin:
+            db.session.delete(group_recipe)
+            db.session.commit()
+            return jsonify({'message': 'Recipe deleted'})
+        else:
+            return jsonify({'error': 'You are not the owner of this recipe'})
+    except Exception as e:
+        return(str(e))
