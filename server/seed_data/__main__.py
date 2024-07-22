8be494653cc51e8640da44f57f60a27aa225c650
Tatiana Stewart <tstwrt7@gmail.com>
2024-07-22 04:00:29 -0400

    Added Seed Data

--- server/seed_data/__main__.py
+++ server/seed_data/__main__.py
@@ -7,7 +7,7 @@ from connection import db
 def seed_groups():
     db.session.query(Group).delete()
 
-    with open('seed_data/seedGroups.json') as f:
+    with open('server/seed_data/seedGroups.json') as f:
         data = json.load(f)
 
         for group in data:
@@ -20,8 +20,29 @@ def seed_groups():
             db.session.add(new_group)
             db.session.commit()
 
+def seed_recipes():
+    db.session.query(Recipe).delete()
 
+    with open('server/seed_data/seedRecipes.json') as f:
+        data = json.load(f)
+
+        for recipe in data:
+            new_recipe = Recipe(
+                name=recipe['name'],
+                ingredients=recipe['ingredients'],
+                instructions=recipe['instructions'],
+                servings= recipe.get('servings', None),
+                time= recipe.get('time', None),
+                image= recipe.get('image', None),
+                tags=recipe['tags'],
+                user=recipe['user'],
+                group_id= recipe.get('group_id', None),
+                is_public= recipe.get('is_public', True)
+            )
+            db.session.add(new_recipe)
+            db.session.commit()
 
 def seedData():
     seed_groups()
+    seed_recipes()
     print('Seeded Data')
